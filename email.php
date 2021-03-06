f18bbde5547bd7d6cf3fd274a05f57ecefeccac1
miguel <arrietaeguren@gmail.com>
2021-03-06 12:30:56 +0100

    working email

--- email.php
+++ email.php
@@ -1,18 +1,18 @@
 <?php
 
-	$to      = 'arrietaeguren@gmail.com';
-	$from    = 'arrietaeguren@gmail.com';
-	$fromName= $_REQUEST['fromName'] or '';
-	//$reply_to= $_REQUEST['reply_to'];
-	$subject = $_REQUEST['subject'] or 'Titulo del mensaje';
-	$message = $_REQUEST['message'] or 'Contenido del mensaje';
+$to = 'arrietaeguren@gmail.com';
+$from = 'seminario@iglesianavarra.org';
+$name = $_REQUEST['name'] or '';
+//$reply_to= $_REQUEST['reply_to'];
+$subject = 'Petición web: rezar por ' . $name;
+$message = $_REQUEST['message'];
+$message = 'Nombre (pseudónimo): ' . $name . "\r\n\r\nPeticion: " . $message;
 
-	$headers = 'From: '. $fromName .' <' . $from . ">\r\n" . 
-	    //'Reply-To: <' . $reply_to . ">\r\n" .
-	    'X-Mailer: PHP/' . phpversion();
+$headers = 'From: ' . $name . ' <' . $from . ">\r\n" . 'X-Mailer: PHP/' . phpversion();
+//'Reply-To: <' . $reply_to . ">\r\n" .
 
-	echo mail($to, $subject, $message, $headers);
+echo mail($to, $subject, $message, $headers);
 
-	//mail("arrietaeguren.es", "Fake mail enviado", $message, $headers);
-	echo "Gracias por email! Cuenta con nuestras oraciones!";
+//mail("arrietaeguren.es", "Fake mail enviado", $message, $headers);
+echo 'Gracias ' . $name . '! Cuenta con nuestras oraciones!';
 ?>
