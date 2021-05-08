cf3b6719e2923494c925e5bb48391ce93646be24
miguel <arrietaeguren@gmail.com>
2021-05-08 17:38:48 +0200

    fix email...

--- email.php
+++ email.php
@@ -4,14 +4,15 @@ $to = 'arrietaeguren@gmail.com';
 $from = 'seminario@iglesianavarra.org';
 $name = $_REQUEST['name'] or '';
 //$reply_to= $_REQUEST['reply_to'];
-$subject = 'Petición web: rezar por ' . $name;
+$subject = 'Petición web: ' . $name;
 $message = $_REQUEST['message'];
-$message = 'Nombre (pseudónimo): ' . $name . "\r\n\r\nPeticion: " . $message;
+$email = $_REQUEST['email'] or '';
+$text = 'Nombre: ' . $name . "\r\n\r\nPeticion: " . $message. "\r\n\r\nEmail: " . $email;
 
 $headers = 'From: ' . $name . ' <' . $from . ">\r\n" . 'X-Mailer: PHP/' . phpversion();
 //'Reply-To: <' . $reply_to . ">\r\n" .
 
-echo mail($to, $subject, $message, $headers);
+echo mail($to, $subject, $text, $headers);
 
 //mail("arrietaeguren.es", "Fake mail enviado", $message, $headers);
 echo 'Gracias ' . $name . '! Cuenta con nuestras oraciones!';
